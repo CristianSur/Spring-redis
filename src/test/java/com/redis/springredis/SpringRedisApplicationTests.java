4ddba29b545fd98a07fba3e393b18337054c4497
Chris <suruceanu.cristian@gmail.com>
2022-05-08 23:56:56 +0300

    Updated structure

--- src/test/java/com/redis/springredis/SpringRedisApplicationTests.java
+++ src/test/java/com/redis/springredis/SpringRedisApplicationTests.java
@@ -1,13 +1,30 @@
 package com.redis.springredis;
 
+import com.redis.springredis.repository.CommentDao;
+import com.redis.springredis.repository.ProductDao;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringRedisApplicationTests  {
+    private final ProductDao dao;
+    private final CommentDao commentDao;
+
+
+    @Autowired
+    SpringRedisApplicationTests(ProductDao dao, CommentDao commentDao) {
+        this.dao = dao;
+        this.commentDao = commentDao;
+    }
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void dataShouldBeSavedInRedis() {
+        System.out.println(dao.findAll());
     }
 
 }
